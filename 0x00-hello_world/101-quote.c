a642bfb13647fbb50b6d8b9cdd86f954c0969799
alexUd01 <alexanderikpeama@gmail.com>
2022-09-08 20:14:58 -0700

    Done with task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,8 @@
 
 /**
  * main - the main function
- * return: returns 1
+ * Return: returns 1
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
