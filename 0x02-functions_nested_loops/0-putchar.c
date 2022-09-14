9a7d3730155e636bdeccf38abbc940b161702de0
alex <alex@Xandex-PC>
2022-09-14 01:11:26 +0100

    Done with task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
- * main - entrypoint of the program
- * Description: this program prints _putchar followed by a new line
- * Return: returns 0 if successful
- */
+* main - prints the string _putchar with headers
+* Return: returns 0
+*/
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
