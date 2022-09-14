888f3d51320ef2699877a1ec8b01aaf1ffe87096
alex <alex@Xandex-PC>
2022-09-14 01:34:25 +0100

    Done with task 3; There may be errors

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * main - the entry point of the program.
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char x = 'a';
+
+	while(x <= z)
+	{
+		_putchar(x);
+		x++;
+	}
+	return (0);
+}
