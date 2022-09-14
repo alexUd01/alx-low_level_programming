18af7ee2bf0b42ab369e73baea175784806f9886
alex <alex@Xandex-PC>
2022-09-14 03:51:28 +0100

    Done with task 5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include"main.h"
 
 /**
- * int _isalpha - checks for alphabetical character
- * Return: returns 1 if c is a letter and 0 if otherwise
+ * _isalpha - checks for alphabetical character
+ * @c: ASCII value will be checked for each character.
+ *Return: returns 1 if it is a letter and 0 if otherwise
  */
 int _isalpha(int c)
 {
-	int c;
 	while ((c > 64) && (c < 91) || (c > 96) && (c < 123))
 		return (1);
 	else
