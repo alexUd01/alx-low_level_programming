5c9987ba4459cddfeec454bbf07d51650c2377d1
alexUd01 <alexanderikpeama@gmail.com>
2022-09-15 15:25:38 +0100

    Just finished Task 2

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - test if "mul" function works
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096));
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers.
+ * @a: the first parameter (integer 1)
+ * @b: the second parameter (integer 2)
+ *
+ * Return: returns m
+ */
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+
+	return (m);
+}
