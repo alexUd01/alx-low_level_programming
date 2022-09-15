f494ab290512d98b6c766a13eba8aabea73aa62a
alexUd01 <alexanderikpeama@gmail.com>
2022-09-15 15:12:27 +0100

    Just finished Task 1

---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - made for the purpose of testing _isdigit function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	c = '0';
+	printf("%c: %d\n", c, _isdigit(c));
+	c = '9';
+	printf("%c: %d\n", c, _isdigit(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isdigit(c));
+
+	return (0);
+}
