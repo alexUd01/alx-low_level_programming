79cd54cda0460fc95b47320dd9a4b315d9680e14
alexUd01 <alexanderikpeama@gmail.com>
2022-09-15 23:14:04 +0100

    Just finished Task 8

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests if print_square function works fine
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_square(2);
+	print_square(10);
+	print_square(0);
+
+	return (0);
+}
