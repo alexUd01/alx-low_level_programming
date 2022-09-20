fee382bfb40af4a26b96d88614b79891f7d1a810
alexUd01 <alexanderikpeama@gmail.com>
2022-09-20 01:39:55 +0100

    Just finished Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * print_array - a function nts n elements of an array of integers,
+ * followed by a new line
+ * @a: the array
+ * @n: the number of elements of the array to be printed
+ */
 void print_array(int *a, int n)
 {
 	int i;
