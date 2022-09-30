e49950b742abcb36930f812df06195aac12127d9
alexUd01 <alexanderikpeama@gmail.com>
2022-09-30 18:39:22 +0100

    Done with task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: an array of the strings passed during runtime
  * Return: always returns 0 (success)
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(int argc, char *argv[]);
 int _putchar(char c);
 
 
