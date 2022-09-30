8ade1d520d9ce005b610da43eb62650cf3115a03
alexUd01 <alexanderikpeama@gmail.com>
2022-09-30 18:55:08 +0100

    Done with task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,9 +2,9 @@
 
 /**
  * main - a program that prints teh number of arguments passed into it
- * @
- *
- *
+ * @argc: the number of arguments passed during runtime of the program
+ * @argv: an array of strings comtaining the strings passed during runtime
+ * Return: returns always
  */
 int main(int argc, __attribute__((unused))char *argv[])
 {
