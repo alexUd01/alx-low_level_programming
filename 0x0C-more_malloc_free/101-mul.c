5ff847665337fcda62868e1d332bb771304981af
alexUd01 <alexanderikpeama@gmail.com>
2022-10-06 21:57:17 +0100

    Done with all tasks

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -16,22 +16,22 @@ int main(int argc, char *argv[])
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	s1=argv[1], s2 = argv[2];
+	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
-	result = malloc(sizeof(*result) * len);
+	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (i = 0; i <= (len1 + len2); i++)
+	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	for (len1 = len1 - 1; len >= 0; len--)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2>= 0; len2--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-	for (i = 0; i < (len - 1); i++)
+	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
 			a = 1;
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
  */
 int is_digit(char *s)
 {
-	int i;
+	int i = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -73,21 +73,23 @@ int is_digit(char *s)
 }
 
 /**
- * _strlen - a function that calculates the length of a string (except '\0')
- * @s: the string whose length is to be measured
- * Return: returns the length of the string
+ * _strlen - calculates the length of a string (excluding the null-terminator)
+ * @s: the string to be checked
+ * Return: returns the length of the string as an int
  */
 int _strlen(char *s)
 {
-	int i, len = 0;
+	int i = 0, c = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
-		len++;
-	return (len);
+		c++;
+
+	return (c);
 }
 
 /**
- * errors - a function that prints error and exits with status 98.
+ * errors - prints out "Error" if an error is encountered and exits with
+ * a status of 98
  * Return: returns nothing
  */
 void errors(void)
