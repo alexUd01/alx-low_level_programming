84ffbd5166e3ce56d874ec74de7257cf717b09f5
alexUd01 <alexanderikpeama@gmail.com>
2022-10-06 16:03:06 +0100

    Work in progress

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *newStr;
 	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
 		return (NULL);
 
 	newStr = malloc(sizeof(*newStr) * nmemb * size);
