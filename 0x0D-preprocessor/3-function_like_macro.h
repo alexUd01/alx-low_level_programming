9e8d8722c320da7e1ab21e86a2969a8cc707919f
alexUd01 <alexanderikpeama@gmail.com>
2022-10-10 14:36:36 +0100

    Task 2

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#include <stdio.h>
+#define ABS(x) (unsigned int x)
+
+#endif
---
+++ 0x0D-preprocessor/back.h
@@ -0,0 +1,7 @@
+#ifndef BACK_H
+#define BACK_H
+
+#include <stdio.h>
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
+#endif
