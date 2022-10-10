680e38b7e9bf75a8e91fa64ce9c8d68310459d58
alexUd01 <alexanderikpeama@gmail.com>
2022-10-10 14:56:40 +0100

    Task 0

--- 0x0E-structures_typdef/dog.h
+++ 0x0E-structures_typdef/dog.h
@@ -2,13 +2,17 @@
 #define DOG_H
 
 #include <stdio.h>
-
+/**
+ * struct dog - a structure containing some elements
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 
 #endif
