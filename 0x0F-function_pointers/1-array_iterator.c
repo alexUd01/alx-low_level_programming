42d2fca4ed84e74568f6c818b88c89691b59d4b5
alexUd01 <alexanderikpeama@gmail.com>
2022-10-12 17:23:41 +0100

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (action == NULL || size <= 0)
+	if (action == NULL || size <= 0|| array == NULL)
 		return;
 	for (i = 0; i < size; i++)
 	{
