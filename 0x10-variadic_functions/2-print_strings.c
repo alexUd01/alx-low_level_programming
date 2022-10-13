b79384cb360c79e3875a357cf90fbaecaf26e2dc
alexUd01 <alexanderikpeama@gmail.com>
2022-10-13 21:46:11 +0100

    Task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,7 +2,7 @@
 
 /**
  * print_strings - a function that prints strings followed by a new line
- * @seperator: the string to be printed between the strings
+ * @separator: the string to be printed between the strings
  * @n: the number of strings passed to the function
  * Return: returns nothing
  */
