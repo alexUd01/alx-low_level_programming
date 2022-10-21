a0091e3ebdca99da1d9ab9cc0953ef1b8a4958e0
alexUd01 <alexanderikpeama@gmail.com>
2022-10-21 02:24:18 +0100

    Task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,7 +9,7 @@
 size_t list_len(const list_t *h)
 {
 	const list_t *ptr = h;
-	size_t i;
+	size_t i = 0;
 
 	if (ptr == NULL)
 		return (0);
@@ -21,3 +21,28 @@ size_t list_len(const list_t *h)
 	}
 	return (i);
 }
+int main(void)
+{
+    list_t *head;
+    list_t *new;
+    list_t hello = {"World", 5, NULL};
+    size_t n;
+
+    head = &hello;
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->str = strdup("Hello");
+    new->len = 5;
+    new->next = head;
+    head = new;
+    n = list_len(head);
+    printf("-> %lu elements\n", n);
+    free(new->str);
+    free(new);
+    return (0);
+}
+
