c938fdcd03ae2c895c6e4ed507163a6ca964957f
alexUd01 <alexanderikpeama@gmail.com>
2022-10-25 16:08:24 +0100

    task 9 correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -45,7 +45,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			temp->next = ptr->next;
 			ptr->next = temp;
-			return (*temp);
+			return (temp);
 		}
 		count2++;
 		ptr = ptr->next;
