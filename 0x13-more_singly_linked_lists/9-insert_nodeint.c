31ac2ee8ace2d5dace453e5c7090b2fbf454f3ed
alexUd01 <alexanderikpeama@gmail.com>
2022-10-25 15:55:53 +0100

    task 9 correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		count++;
 		ptr = ptr->next;
 	}
-	if (idx > count)
+	if (idx >= count)
 		return (NULL);
 
 	temp = malloc(sizeof(listint_t));
