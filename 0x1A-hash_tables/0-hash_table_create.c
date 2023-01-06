a9238b3000c2dfe7c16bb75aeee8de094f98ca14
alexUd01 <alexanderikpeama@gmail.com>
2023-01-06 16:42:41 +0100

    correction Task 2 and Task 3: hash table set

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
@@ -16,6 +17,21 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
+
+	/* allocate memory for hash table array of items */
+	new_table->array = malloc(sizeof(hash_node_t) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	/* initialize the value of size */
+	new_table->size = size;
+
+	/* initialize elements of hash table with NULL */
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 
 	return (new_table);
 }
