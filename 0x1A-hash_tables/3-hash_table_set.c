35d9cd3e072cc16d6a22dc87958512ee1c188dc3
alexUd01 <alexanderikpeama@gmail.com>
2023-01-06 23:32:19 +0100

    correction of task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -55,24 +55,53 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	/* case 2: collision occured */
-	if (ht->array[index] != NULL)
+	/* case 2a: replace value if key exists in linked list */
+	ptr = ht->array[index];
+	while (ptr != NULL)
 	{
-		/* replace value if key exists in linked list */
-		ptr = ht->array[index];
-		while (ptr != NULL)
+		if (strcmp(ptr->key, key) == 0)
 		{
-			if (strcmp(ptr->key, key) == 0)
-			{
-				ptr->value = strdup(value);
-				free(temp->value), free(temp), temp = NULL;
-				return (1);
-			}
-			ptr = ptr->next;
+			ptr->value = strdup(value);
+			free(temp->value), free(temp), temp = NULL;
+			return (1);
 		}
-		/* add to top of bucket if keys are different */
-		temp->next = ht->array[index];
-		ht->array[index] = temp;
-		return (1);
+		ptr = ptr->next;
 	}
-	return (0);
+	/* case 2b: add to top of bucket if keys are different */
+	temp->next = ht->array[index];
+	ht->array[index] = temp;
+	return (1);
+}
+int main(void)
+{
+    hash_table_t *ht;
+    char *value;
+
+    ht = hash_table_create(1024);
+    hash_table_set(ht, "c", "fun");
+    hash_table_set(ht, "python", "awesome");
+    hash_table_set(ht, "Bob", "and Kris love asm");
+    hash_table_set(ht, "N", "queens");
+    hash_table_set(ht, "Asterix", "Obelix");
+    hash_table_set(ht, "Betty", "Cool");
+    hash_table_set(ht, "98", "Battery Street");
+    hash_table_set(ht, "c", "isfun");
+
+    value = hash_table_get(ht, "python");
+    printf("%s:%s\n", "python", value);
+    value = hash_table_get(ht, "Bob");
+    printf("%s:%s\n", "Bob", value);
+    value = hash_table_get(ht, "N");
+    printf("%s:%s\n", "N", value);
+    value = hash_table_get(ht, "Asterix");
+    printf("%s:%s\n", "Asterix", value);
+    value = hash_table_get(ht, "Betty");
+    printf("%s:%s\n", "Betty", value);
+    value = hash_table_get(ht, "98");
+    printf("%s:%s\n", "98", value);
+    value = hash_table_get(ht, "c");
+    printf("%s:%s\n", "c", value);
+    value = hash_table_get(ht, "javascript");
+    printf("%s:%s\n", "javascript", value);
+    return (EXIT_SUCCESS);
 }
