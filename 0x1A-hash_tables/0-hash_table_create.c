0a68a1f2d47192266467fd83b01d3a751a07f946
alexUd01 <alexanderikpeama@gmail.com>
2023-01-06 13:16:50 +0100

    Task 0: create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,48 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - a function that creates a hash table
+ * @size: the size of the array
+ * Return: a pointer to the newly created hash table or NULL(ON FAILURE)
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+	unsigned int i;
+
+	/* create new hash table */
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
+	/* allocate memory for hash table array of items */
+	new_table->array = malloc(sizeof(hash_node_t) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	/* initialize the value of size */
+	new_table->size = size;
+
+	/* initialize elements of hash table with NULL */
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
+
+	return (new_table);
+}
+
+/**
+ * main - check the code for
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    printf("%p\n", (void *)ht);
+    return (EXIT_SUCCESS);
+}
