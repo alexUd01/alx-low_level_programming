dc3699133be248ed05a15b0b0f43d2daf2b72ebe
alexUd01 <alexanderikpeama@gmail.com>
2023-01-06 16:50:31 +0100

    correction Task 2 and Task 3: hash table set

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 	unsigned int i;
 
-	if (size == 0)
+	if (size == 0 || size > ULONG_MAX)
 		return (NULL);
 
 	/* create new hash table */
